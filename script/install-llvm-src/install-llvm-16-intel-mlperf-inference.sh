184d0432077000de61a6b6dc67a278da5972ae09
Arjun <arjunsuresh1987@gmail.com>
2024-05-14 14:29:42 +0000

    Improve dependency handling for llvm-install intel-mlperf-inference

--- script/install-llvm-src/install-llvm-16-intel-mlperf-inference.sh
+++ script/install-llvm-src/install-llvm-16-intel-mlperf-inference.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 export PATH=${CM_CONDA_BIN_PATH}:${PATH}
-export ABI=$(python -c "import torch; print(int(torch._C._GLIBCXX_USE_CXX11_ABI))")
+#export LD_LIBRARY_PATH=${CM_CONDA_LIB_PATH}:${LD_LIBRARY_PATH}
+ABI=$(python -c "import torch; print(int(torch._C._GLIBCXX_USE_CXX11_ABI))")
+test $? -eq 0 || exit $?
+export ABI=$ABI
 mkdir -p llvm-project && cd llvm-project
 wget -nc https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.6/cmake-16.0.6.src.tar.xz
 wget -nc https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.6/llvm-16.0.6.src.tar.xz
@@ -14,6 +17,8 @@ rm -rf build
 mkdir -p build
 cd build
 export DEB_BUILD_MAINT_OPTIONS=hardening=-format
+export CC=${CM_C_COMPILER_WITH_PATH}
+export CXX=${CM_CXX_COMPILER_WITH_PATH}
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${ABI}" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_BUILD_LLVM_DYLIB=ON   -DLLVM_INCLUDE_BENCHMARKS=OFF ../llvm/
 
 test $? -eq 0 || exit $?
