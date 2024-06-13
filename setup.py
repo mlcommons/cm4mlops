92dcc932f93151e227e9faa2480c39c23a11b9d1
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-06-13 22:56:35 +0530

    fix the wheel build

--- setup.py
+++ setup.py
@@ -111,10 +111,12 @@ setup(
     version='0.1',
     packages=[],
     setup_requires=[
+        "wheel",
         "cmind @ git+https://git@github.com/mlcommons/ck.git@a4c6a7b477af5f1e7099c55f5468a47854adaa6c#egg=cmind&subdirectory=cm"
         ],
     install_requires=[
         "wheel",
+        "cmind @ git+https://git@github.com/mlcommons/ck.git@a4c6a7b477af5f1e7099c55f5468a47854adaa6c#egg=cmind&subdirectory=cm"
         "giturlparse",
         "requests",
         "pyyaml",
