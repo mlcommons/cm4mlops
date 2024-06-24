967aa49d037299afabcbb7f5edf961bd02a22982
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-06-24 18:58:21 +0530

    Fixed rclone issue

--- script/get-dataset-mixtral/run.bat
+++ script/get-dataset-mixtral/run.bat
@@ -1,8 +1,6 @@
 echo.
 
-@REM Powershell.exe -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-noexit -noprofile -ExecutionPolicy Bypass -Command ""rclone copyurl %CM_RCLONE_WINDOWS_URL% . -a -P""'}" 
-
-wget %CM_RCLONE_WINDOWS_URL% --no-check-certificate
+rclone copyurl %CM_RCLONE_WINDOWS_URL% . -a -P
 IF %ERRORLEVEL% NEQ 0 EXIT 1
 
 echo CM_DATASET_PREPROCESSED_PATH=%CD%\%CM_DATASET_FILE_NAME% > tmp-run-env.out
