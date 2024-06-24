4c090b623482550285cea86dd57377045a841c09
anandhu-eng <anandhukicks@gmail.com>
2024-06-24 17:08:36 +0530

    Made dataset download for linux and windows working

--- script/get-dataset-mixtral/run.sh
+++ script/get-dataset-mixtral/run.sh
@@ -2,14 +2,7 @@
 
 echo ""
 
-wget -4 -nc ${CM_WGET_URL} --no-check-certificate
+rclone copyurl ${CM_RCLONE_LINUX_URL} ./ -a -P
 test $? -eq 0 || exit 1
 
-mkdir data
-
-tar -C data -xvzf caffe_ilsvrc12.tar.gz
-test $? -eq 0 || exit 1
-
-rm -rf caffe_ilsvrc12.tar.gz
-
-echo "CM_DATASET_AUX_PATH=$PWD/data" > tmp-run-env.out
+echo "CM_DATASET_PREPROCESSED_PATH=$PWD/${CM_DATASET_FILE_NAME}" > tmp-run-env.out
