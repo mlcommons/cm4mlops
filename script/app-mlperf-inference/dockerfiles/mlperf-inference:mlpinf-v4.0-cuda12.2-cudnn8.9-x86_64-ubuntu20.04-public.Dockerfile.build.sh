5786862cbd6aee4d2d4a8dd3d35e9ec972955b7b
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-07-01 19:37:56 +0530

    Delete script/app-mlperf-inference/dockerfiles directory

--- script/app-mlperf-inference/dockerfiles/mlperf-inference:mlpinf-v4.0-cuda12.2-cudnn8.9-x86_64-ubuntu20.04-public.Dockerfile.build.sh
+++
@@ -1,5 +0,0 @@
-docker build  \
- --build-arg GID=\" $(id -g $USER) \" --build-arg UID=\" $(id -u $USER) \" \
- -f "/home/anandhu/CM/repos/anandhu-eng@cm4mlops/script/app-mlperf-inference/dockerfiles/mlperf-inference:mlpinf-v4.0-cuda12.2-cudnn8.9-x86_64-ubuntu20.04-public.Dockerfile" \
- -t "cknowledge/cm-script-app-mlperf-inference:ubuntu-20.04-latest" \
- .
--- script/app-mlperf-inference/dockerfiles/pytorch:24.03-py3.Dockerfile.build.bat
+++
@@ -1,5 +0,0 @@
-docker build  ^
- --build-arg GID=\" $(id -g $USER) \" --build-arg UID=\" $(id -u $USER) \" ^
- -f "/home/anandhu/CM/repos/anandhu-eng@cm4mlops/script/app-mlperf-inference/dockerfiles/pytorch:24.03-py3.Dockerfile" ^
- -t "cknowledge/cm-script-app-mlperf-inference:ubuntu-22.04-latest" ^
- .
