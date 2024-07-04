30123f0352a1d04417a361d8db05c42c35cc2ca0
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-07-04 15:34:24 +0530

    Updation for quantisation of llama2

--- script/get-ml-model-llama2/customize.py
+++ script/get-ml-model-llama2/customize.py
@@ -6,17 +6,22 @@ def preprocess(i):
     os_info = i['os_info']
     env = i['env']
 
-    path = env.get('LLAMA2_CHECKPOINT_PATH', '').strip()
+    if env.get('CM_TMP_ML_MODEL_PROVIDER', '') == 'nvidia':
+        i['run_script_input']['script_name'] = 'run-nvidia'
+        gpu_arch = int(float(env['CM_CUDA_DEVICE_PROP_GPU_COMPUTE_CAPABILITY']) * 10)
+        env['CM_GPU_ARCH'] = gpu_arch
+        env['CM_TMP_REQUIRE_DOWNLOAD'] = 'no'
+    else:
+        path = env.get('LLAMA2_CHECKPOINT_PATH', '').strip()
 
-    if path == '' or not os.path.exists(path):
-        env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
+        if path == '' or not os.path.exists(path):
+            env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
 
-    return {'return':0}
+        return {'return':0}
 
 def postprocess(i):
 
     env = i['env']
-
     if env.get('LLAMA2_CHECKPOINT_PATH', '' ) ==  '':
         env['LLAMA2_CHECKPOINT_PATH'] = env['CM_ML_MODEL_PATH']
 
