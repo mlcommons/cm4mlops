3c43a66d773613bf6bd5a3de0d3af85d7bbc7c8c
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-07-04 19:24:28 +0530

    Added install failure check for install-cuda

--- script/install-cuda-prebuilt/run.sh
+++ script/install-cuda-prebuilt/run.sh
@@ -7,4 +7,4 @@ INSTALL_DIR=${CUR}/install
 cmd="${CM_SUDO} bash ${CM_CUDA_RUN_FILE_PATH} --toolkitpath=${INSTALL_DIR} --defaultroot=${INSTALL_DIR} --toolkit ${CUDA_ADDITIONAL_INSTALL_OPTIONS} --silent --override"
 echo "${cmd}"
 eval "${cmd}"
-
+test $? -eq 0 || exit $?
