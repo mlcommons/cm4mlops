f6b8f5c7d8dc763fb6bf4ad5ffece1facd25eb34
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-07-05 20:16:46 +0000

    Intel mlperf inference dlrm v2 (WIP)

--- script/install-ipex-from-src/run.sh
+++ script/install-ipex-from-src/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export PATH=${CM_CONDA_BIN_PATH}:${PATH}
+#export PATH=${CM_CONDA_BIN_PATH}:${PATH}
+#echo $LD_LIBRARY_PATH
+#exit 1
+#export LD_LIBRARY_PATH=""
 
 rm -rf ipex_src
 cp -r ${IPEX_DIR} ipex_src
@@ -24,6 +27,21 @@ elif [[ ${CM_INTEL_IPEX_3D_UNET_PATCH} == "yes" ]]; then
   cd ../../
   bash ${CM_TMP_CURRENT_SCRIPT_PATH}/apply_intel_3d-unet_patch.sh
   test "$?" -eq 0 || exit "$?"
+
+elif [[ ${CM_INTEL_IPEX_DLRM_V2_PATCH} == "yes" ]]; then
+  cd third_party/libxsmm
+  git checkout c21bc5ddb4
+  test "$?" -eq 0 || exit "$?"
+  cd ../ideep && rm -rf mkl-dnn && git checkout b5eadff696
+  test "$?" -eq 0 || exit "$?"
+  git submodule sync && git submodule update --init --recursive
+  test "$?" -eq 0 || exit "$?"
+  cd mkl-dnn
+  wget https://raw.githubusercontent.com/mlcommons/inference_results_v3.1/main/closed/Intel/code/dlrm-v2-99/pytorch-cpu-int8/onednngraph.patch
+  test "$?" -eq 0 || exit "$?"
+  git apply -p1 onednngraph.patch
+  test "$?" -eq 0 || exit "$?"
+  cd ../../../
 fi
 
 rm -rf build
