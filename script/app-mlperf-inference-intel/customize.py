f2aa787efbc99d5c16fa61eca399fd8cc833d825
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-07-08 12:39:32 +0000

    Fixes for mlperf inference intel dlrmv2 (run starts)

--- script/app-mlperf-inference-intel/customize.py
+++ script/app-mlperf-inference-intel/customize.py
@@ -31,6 +31,8 @@ def preprocess(i):
     if env.get('CM_MLPERF_INFERENCE_CODE_VERSION', '') == 'v4.0':
         if 'gptj' in ml_model:
             code_base_folder = "ITREX"
+    if 'dlrm-v2' in ml_model:
+        code_base_folder = "pytorch-cpu-int8"
 
     harness_root = os.path.join(env['CM_MLPERF_INFERENCE_RESULTS_PATH'], 'closed', 'Intel', 'code', ml_model, code_base_folder)
 
@@ -161,6 +163,10 @@ def preprocess(i):
         elif '3d-unet' in env['CM_MODEL']:
             env['CM_RUN_DIR'] = env['CM_MLPERF_OUTPUT_DIR']
             env['CM_RUN_CMD'] = f"bash {os.path.join(i['run_script_input']['path'],'run_3d-unet_harness.sh')} "
+
+        elif 'dlrm' in env['CM_MODEL']:
+            env['CM_RUN_DIR'] = i['run_script_input']['path']
+            env['CM_RUN_CMD'] = f"bash {os.path.join(i['run_script_input']['path'],'run_dlrm_v2_harness.sh')} "
 
         elif "gptj" in env['CM_MODEL']:
             env['CM_RUN_DIR'] = i['run_script_input']['path']
