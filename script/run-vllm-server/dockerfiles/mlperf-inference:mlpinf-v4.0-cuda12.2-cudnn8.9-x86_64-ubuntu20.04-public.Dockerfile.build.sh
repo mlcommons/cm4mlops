b5082ff6bd14ffed1055471a7cf8a995381277ab
anandhu-eng <anandhukicks@gmail.com>
2024-07-17 23:10:34 +0530

    added all vllm server api arguments

---
+++ script/run-vllm-server/dockerfiles/mlperf-inference:mlpinf-v4.0-cuda12.2-cudnn8.9-x86_64-ubuntu20.04-public.Dockerfile.build.sh
@@ -0,0 +1,5 @@
+docker build  \
+ --build-arg GID=\" $(id -g $USER) \" --build-arg UID=\" $(id -u $USER) \" \
+ -f "/home/anandhu/CM/repos/anandhu-eng@cm4mlops/script/run-vllm-server/dockerfiles/mlperf-inference:mlpinf-v4.0-cuda12.2-cudnn8.9-x86_64-ubuntu20.04-public.Dockerfile" \
+ -t "cknowledge/cm-script-run-vllm-server:ubuntu-22.04-latest" \
+ .
---
+++ script/run-vllm-server/dockerfiles/ubuntu_22.04.Dockerfile.build.bat
@@ -0,0 +1,5 @@
+docker build  --no-cache ^
+ --build-arg GID=\" $(id -g $USER) \" --build-arg UID=\" $(id -u $USER) \" ^
+ -f "/home/anandhu/CM/repos/anandhu-eng@cm4mlops/script/run-vllm-server/dockerfiles/ubuntu_22.04.Dockerfile" ^
+ -t "cknowledge/cm-script-run-vllm-server:ubuntu-22.04-latest" ^
+ .
