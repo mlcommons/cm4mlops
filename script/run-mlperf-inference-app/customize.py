16bae5bdba565e72061a0e348f79512aeb6dd7aa
anandhu-eng <anandhukicks@gmail.com>
2024-07-20 10:47:46 +0530

    fixed typos

--- script/run-mlperf-inference-app/customize.py
+++ script/run-mlperf-inference-app/customize.py
@@ -111,7 +111,7 @@ def preprocess(i):
     variation_device= ",_" + env["CM_MLPERF_DEVICE"] if env.get("CM_MLPERF_DEVICE","") != "" else ""
     variation_run_style= ",_" + env.get("CM_MLPERF_RUN_STYLE", "test")
     variation_reproducibility= ",_" + env["CM_RUN_MLPERF_INFERENCE_APP_DEFAULTS"] if env.get("CM_RUN_MLPERF_INFERENCE_APP_DEFAULTS","") != "" else ""
-    variation_all_models= ",_all_models" if env.get("CM_MLPERF_ALL_MODELS"."") == "yes" else ""
+    variation_all_models= ",_all_models" if env.get("CM_MLPERF_ALL_MODELS","") == "yes" else ""
     
     if env.get("CM_MLPERF_MODEL_PRECISION", '') != '':
         variation_quantization_string= ",_" + env["CM_MLPERF_MODEL_PRECISION"]
