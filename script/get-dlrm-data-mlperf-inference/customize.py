f0bd46325bc22c7b26009f8ef1fc9d417aaef263
anandhu-eng <anandhukicks@gmail.com>
2024-08-08 02:24:14 +0530

    fixed folder structure + generation of run cmd from customize.py

--- script/get-dlrm-data-mlperf-inference/customize.py
+++ script/get-dlrm-data-mlperf-inference/customize.py
@@ -8,9 +8,22 @@ def preprocess(i):
     env = i['env']
 
     dlrm_data_path = env.get('CM_DLRM_DATA_PATH', env.get('DLRM_DATA_PATH', ''))
-    if dlrm_data_path == '' or not os.path.exists(dlrm_data_path):
-        print(f'Data path is not given as input through --dlrm_data_path. Using the cache directory:{os.getcwd()} as the data path'}
+    if dlrm_data_path == '':
+        print(f'Data path is not given as input through --dlrm_data_path. Using the cache directory:{os.getcwd()} as the data path')
         dlrm_data_path = os.getcwd()
+    elif not os.path.exists(dlrm_data_path):
+        return {'return':1, 'error':"given dlrm data path does not exists"}
+    
+    # creating required folders inside the dlrm data path if not exists
+    # criteo dataset
+    criteo_fp32_path = os.path.join(dlrm_data_path, "criteo", "day23", "fp32")
+    if not os.path.exists(criteo_fp32_path):
+        os.makedirs(criteo_fp32_path)
+
+    # dlrm model
+    model_path = os.path.join(dlrm_data_path, "model")
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
 
     meta = i['meta']
 
@@ -47,29 +60,38 @@ def preprocess(i):
         if not os.path.exists(os.path.join(dlrm_data_path, "criteo", "day23", "fp32", "day_23_labels.npy")):
             print(f'day_23_labels.npy is missing inside {dlrm_data_path}/criteo/day23/fp32')
             env['CM_DLRM_DATASET_DOWNLOAD'] = True
-        if not os.path.exists(os.path.join(dlrm_data_path, "criteo", "day23", "raw_data")):
-            print(f'raw_data is missing inside {dlrm_data_path}/criteo/day23')
-            env['CM_DLRM_DATASET_DOWNLOAD'] = True
+        # TBD: raw data is not available with mlcommons hosted dataset
+        #if not os.path.exists(os.path.join(dlrm_data_path, "criteo", "day23", "raw_data")):
+        #    print(f'raw_data is missing inside {dlrm_data_path}/criteo/day23')
+        #    env['CM_DLRM_DATASET_DOWNLOAD'] = True
         
         run_cmd = ''
         xsep = ' && '
 
-        if env['CM_DLRM_DATASET_DOWNLOAD'] != True:
+        # addition of run command to download the datasets and model
+        if env.get('CM_DLRM_DATASET_DOWNLOAD', False) == True:
+            run_cmd += 'cp -r "$CM_CRITEO_PREPROCESSED_PATH"/. ' + os.path.join(dlrm_data_path,"criteo","day23","fp32") + xsep
+        if env.get('CM_DLRM_MODEL_DOWNLOAD', False) == True:
+            run_cmd += 'cp -r "$CM_ML_MODEL_FILE_WITH_PATH"/. ' + os.path.join(dlrm_data_path, "model") + xsep
+
+        if env.get('CM_DLRM_DATASET_DOWNLOAD', '') != True:
             if not os.path.exists(os.path.join(dlrm_data_path, "criteo", "day23", "fp32", "day_23_sparse_multi_hot_unpacked")):
                 os.system(f"unzip {os.path.join(dlrm_data_path, 'criteo', 'day23', 'fp32', 'day_23_sparse_multi_hot.npz')} -d {os.path.join(dlrm_data_path, 'criteo', 'day23', 'fp32', 'day_23_sparse_multi_hot_unpacked')}")
-
+        else:
+            run_cmd += f"unzip {os.path.join(dlrm_data_path, 'criteo', 'day23', 'fp32', 'day_23_sparse_multi_hot.npz')} -d {os.path.join(dlrm_data_path, 'criteo', 'day23', 'fp32', 'day_23_sparse_multi_hot_unpacked')}" + xsep
+        
         if os.path.exists(os.path.join(dlrm_data_path, "criteo", "day23", "fp32", "day_23_sparse_multi_hot.npz")) or env['CM_DLRM_DATASET_DOWNLOAD'] == True:
             file_path = os.path.join(dlrm_data_path, "criteo", "day23", "fp32", "day_23_sparse_multi_hot.npz")
-            run_cmd = xsep + ("echo {} {} | md5sum -c").format('c46b7e31ec6f2f8768fa60bdfc0f6e40', file_path)
+            run_cmd += ("echo {} {} | md5sum -c").format('c46b7e31ec6f2f8768fa60bdfc0f6e40', file_path) + xsep
         
         file_path = os.path.join(dlrm_data_path, "criteo", "day23", "fp32", "day_23_dense.npy")
-        run_cmd += xsep + ("echo {} {} | md5sum -c").format('cdf7af87cbc7e9b468c0be46b1767601', file_path)
+        run_cmd += ("echo {} {} | md5sum -c").format('cdf7af87cbc7e9b468c0be46b1767601', file_path) + xsep
 
         file_path = os.path.join(dlrm_data_path, "criteo", "day23", "fp32", "day_23_labels.npy")
-        run_cmd += xsep + ("echo {} {} | md5sum -c").format('dd68f93301812026ed6f58dfb0757fa7', file_path)
+        run_cmd += ("echo {} {} | md5sum -c").format('dd68f93301812026ed6f58dfb0757fa7', file_path) + xsep
 
         dir_path = os.path.join(dlrm_data_path, "criteo", "day23", "fp32")
-        run_cmd += xsep + ("cd {}; md5sum -c {}").format(dir_path, os.path.join(script_path, "checksums.txt" ))
+        run_cmd += ("cd {}; md5sum -c {}").format(dir_path, os.path.join(script_path, "checksums.txt" ))
 
         env['CM_DLRM_V2_DAY23_FILE_PATH'] = os.path.join(dlrm_data_path, "criteo", "day23", "raw_data")
         env['CM_DLRM_V2_AGGREGATION_TRACE_FILE_PATH'] = os.path.join(dlrm_data_path, "criteo", "day23", "sample_partition.txt")
