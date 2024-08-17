a53553f24d31ce2c39240fbeb77eb2f4ab8969a4
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-08-18 00:45:26 +0530

    Always clean the old git repo

--- script/get-git-repo/run.sh
+++ script/get-git-repo/run.sh
@@ -5,7 +5,7 @@ echo "$CUR_DIR"
 SCRIPT_DIR=${CM_TMP_CURRENT_SCRIPT_PATH}
 
 folder=${CM_GIT_CHECKOUT_FOLDER}
-if [ ! -d "${CM_TMP_GIT_PATH}" ]; then
+if [ ! -d "${CM_TMP_GIT_PATH}" && 0 ]; then
   rm -rf ${folder}
   echo "******************************************************"
   echo "Current directory: ${CUR_DIR}"
