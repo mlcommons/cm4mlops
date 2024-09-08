687317cd58ad8e3ad5ab238e6e59f667b6704ef7
anandhu-eng <anandhusooraj011@gmail.com>
2024-09-08 01:45:33 +0000

    sdxl v4.1 - WIP

--- script/build-mlperf-inference-server-nvidia/customize.py
+++ script/build-mlperf-inference-server-nvidia/customize.py
@@ -21,7 +21,9 @@ def preprocess(i):
     if env.get('CM_GCC_VERSION', '') != '':
         gcc_major_version = env['CM_GCC_VERSION'].split(".")[0]
         if int(gcc_major_version) > 10:
-            cxxflags.append("-Wno-error=range-loop-construct")
+            print(env.get('CM_MLPERF_INFERENCE_VERSION',''))
+            if env.get('CM_MLPERF_INFERENCE_VERSION','') != "4.1":
+                cxxflags.append("-Wno-error=range-loop-construct")
 
     if env.get('CM_MLPERF_DEVICE','') == "inferentia":
         env['USE_INFERENTIA'] = "1"
