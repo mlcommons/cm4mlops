4e924dada2a8c7d7240a6a22c17ef6af34347b76
GitHub Action <admin@gateoverflow.com>
2024-10-04 18:21:13 +0000

    Results from R50 GH action

---
+++ tmp-run.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set PATH=C:\Users\runneradmin\CM\repos\local\cache\9eb6b38574944d11\bin;C:\hostedtoolcache\windows\Python\3.12.6\x64\Scripts;%PATH%
+set CM_GIT_BRANCH=main
+set CM_GIT_CHECKOUT_PATH=C:\Users\runneradmin\CM\repos\local\cache\d50878e5baee4207\repo
+set CM_GIT_REPO_CHECKOUT=main
+set CM_GIT_REPO_CHECKOUT_FOLDER=repo
+set CM_GIT_REPO_CHECKOUT_PATH=C:\Users\runneradmin\CM\repos\local\cache\d50878e5baee4207\repo
+set CM_GIT_REPO_DEPTH=--depth 5
+set CM_GIT_REPO_NAME=mlperf_inference_test_submissions_v5.0
+set CM_GIT_REPO_PATCH=no
+set CM_GIT_REPO_RECURSE_SUBMODULES= --recurse-submodules
+set CM_GIT_REPO_URL=https://github.com/gateoverflow/mlperf_inference_test_submissions_v5.0
+set CM_MLPERF_INFERENCE_SUBMISSION_DIR=C:\Users\runneradmin\CM\repos\local\cache\1d885a33c39a4564\mlperf-inference-submission
+set CM_MLPERF_INFERENCE_SUBMISSION_VERSION=4_1-dev
+set CM_MLPERF_RESULTS_GIT_REPO_URL=https://github.com/gateoverflow/mlperf_inference_test_submissions_v5.0
+set CM_MLPERF_RESULTS_REPO_COMMIT_MESSAGE=Results from R50 GH action
+set CM_PYTHON_BIN=python.exe
+set CM_PYTHON_BIN_PATH=C:\hostedtoolcache\windows\Python\3.12.6\x64
+set CM_PYTHON_BIN_WITH_PATH=C:\hostedtoolcache\windows\Python\3.12.6\x64\python.exe
+set CM_PYTHON_CACHE_TAGS=version-3.12.6,non-virtual
+set CM_PYTHON_MAJOR_VERSION=3
+set CM_PYTHON_MINOR_VERSION=12
+set CM_PYTHON_PATCH_VERSION=6
+set CM_PYTHON_VERSION=3.12.6
+set CM_QUIET=yes
+set CM_TMP_CURRENT_PATH=D:\a\cm4mlops\cm4mlops
+set CM_TMP_CURRENT_SCRIPT_PATH=C:\Users\runneradmin\CM\repos\anandhu-eng@cm4mlops\script\push-mlperf-inference-results-to-github
+set CM_TMP_CURRENT_SCRIPT_REPO_PATH=C:\Users\runneradmin\CM\repos\anandhu-eng@cm4mlops
+set CM_TMP_CURRENT_SCRIPT_REPO_PATH_WITH_PREFIX=C:\Users\runneradmin\CM\repos\anandhu-eng@cm4mlops
+set CM_TMP_CURRENT_SCRIPT_WORK_PATH=D:\a\cm4mlops\cm4mlops
+set CM_TMP_PIP_VERSION_STRING=
+set CM_WINDOWS=yes
+
+
+call "C:\Users\runneradmin\CM\repos\anandhu-eng@cm4mlops\script\push-mlperf-inference-results-to-github\run.bat"
