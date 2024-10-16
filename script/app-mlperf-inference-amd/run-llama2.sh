fa2eacfdf632d6b16675eee5bb00c98a7ecd601f
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-10-16 19:21:23 +0530

    fix syntax error

--- script/app-mlperf-inference-amd/run-llama2.sh
+++ script/app-mlperf-inference-amd/run-llama2.sh
@@ -33,7 +33,7 @@ LOG_DIR=${CM_MLPERF_OUTPUT_DIR}
 
 cp $USER_CONF ${LOG_DIR}/user.conf
 
-cmd ="${CM_PYTHON_BIN_WITH_PATH} ${CM_MLPERF_AMD_LLAMA2_CODE_PATH}/mainVllmFp8_Offline.py \
+cmd="${CM_PYTHON_BIN_WITH_PATH} ${CM_MLPERF_AMD_LLAMA2_CODE_PATH}/mainVllmFp8_Offline.py \
     --scenario ${CM_MLPERF_LOADGEN_SCENARIO \
     --output-log-dir ${LOG_DIR} \
     --model-path $MODEL_PATH \
