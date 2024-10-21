2ba815e1d5d974215d4e76e09250e9ba92331ffb
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-10-22 02:20:29 +0530

    Export VERSION for mlperf inference src

--- script/get-mlperf-inference-src/customize.py
+++ script/get-mlperf-inference-src/customize.py
@@ -101,6 +101,11 @@ def postprocess(i):
     env['+PYTHONPATH']=[]
     env['+PYTHONPATH'].append(os.path.join(env['CM_MLPERF_INFERENCE_CLASSIFICATION_AND_DETECTION_PATH'], 'python'))
 
+    if os.path.exists(os.path.join(inference_root, "loadgen", "VERSION")):
+        with open(os.path.join(inference_root, "loadgen", "VERSION") as f:
+            version_info = f.read()
+        env['CM_MLPERF_INFERENCE_SOURCE_VERSION'] = version_info
+
     if env.get('CM_GET_MLPERF_IMPLEMENTATION_ONLY', '') == "yes":
         return {'return':0}
 
