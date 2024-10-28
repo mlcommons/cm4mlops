c26a9e5a7619a64c73a7f2bc3f55ab62991cceb9
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-10-29 02:12:47 +0530

    WIP - handle workload of different OS through different scripts

--- script/get-platform-details/customize.py
+++ script/get-platform-details/customize.py
@@ -13,10 +13,8 @@ def preprocess(i):
     os_info = i['os_info']
     env = i['env']
 
-    if os_info['platform'] != "linux":
-        print("WARNING: get-platform-details is not yet supported for non linux systems. Skipping for now...")
-        return {'return': 0}
-    #    return {'return':1, 'error':'get-platform-details script not yet supported in windows!'}
+    if os_info['platform'] == "darwin":
+        i['run_script_input']['script_name'] = "run-macos"
 
     if not check_installation("numactl",os_info):
         env['CM_INSTALL_NUMACTL'] = 'True'
