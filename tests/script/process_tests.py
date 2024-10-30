b1fef06ac04452388b49b52357238f7a81f119d5
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-10-30 21:53:32 +0530

    Fix docker_base_image for CM tests

--- tests/script/process_tests.py
+++ tests/script/process_tests.py
@@ -19,7 +19,18 @@ for file in files:
         data = json.load(f)
     elif file.endswith(".yaml"):
         data = yaml.safe_load(f)
+    if data.get('uid', '') == '':
+        continue #not a CM script meta
     uid = data['uid']
 
-    r = cm.access({'action':'test', 'automation':'script', 'artifact': uid, 'quiet': 'yes', 'out': 'con'})
+    ii = {
+            'action':'test', 'automation':'script', 'artifact': uid, 'quiet': 'yes', 'out': 'con'
+            }
+    if os.environ.get('CM_DOCKER_REPO', '') != '':
+        ii['cm_docker_repo'] = env['CM_DOCKER_REPO']
+    if os.environ.get('CM_DOCKER_REPO_BRANCH', '') != '':
+        ii['cm_docker_repo_branch'] = env['CM_DOCKER_REPO_BRANCH']
+
+    r = cm.access(ii)
+
     checks.check_return(r)
