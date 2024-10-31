6723d2c2f69329cb84654e38c8670db7ee2a0b93
Oseltamvir <bryansg2013@gmail.com>
2024-10-31 17:24:15 +0000

    - fixed an annoying psmisc/pstree bug

--- script/get-generic-sys-util/customize.py
+++ script/get-generic-sys-util/customize.py
@@ -100,25 +100,32 @@ def preprocess(i):
     return {'return':0}
 
 
+
+
 def detect_version(i):
     env = i['env']
     version_env_key = f"CM_{env['CM_SYS_UTIL_NAME'].upper()}_VERSION"
     version_check_re = env.get('CM_SYS_UTIL_VERSION_RE', '')
 
+    # Confirm that the regex pattern and file are present
     if version_check_re == '' or not os.path.exists("tmp-ver.out"):
         version = "undetected"
-
     else:
-        r = i['automation'].parse_version({'match_text': version_check_re,
-                                       'group_number': 1,
-                                       'env_key': version_env_key,
-                                       'which_env': env})
-        if r['return'] >0: return r
+        r = i['automation'].parse_version({
+            'match_text': version_check_re,
+            'group_number': 1,             
+            'env_key': version_env_key,
+            'which_env': env,
+        })
+
+        if r['return'] > 0:
+            return r
 
         version = r['version']
+        print(i['recursion_spaces'] + '    Detected version: {}'.format(version))
 
-        print (i['recursion_spaces'] + '    Detected version: {}'.format(version))
-    return {'return':0, 'version':version}
+    return {'return': 0, 'version': version}
+
 
 def postprocess(i):
     env = i['env']
--- script/get-generic-sys-util/detect.sh
+++ script/get-generic-sys-util/detect.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if [[ -n "${CM_SYS_UTIL_VERSION_CMD}" ]]; then
-  ${CM_SYS_UTIL_VERSION_CMD}  > tmp-ver.out
+  if [[ "${CM_SYS_UTIL_VERSION_CMD}" == *"pstree --version"* ]]; then
+    # Redirect both stdout and stderr to tmp-ver.out
+    ${CM_SYS_UTIL_VERSION_CMD}  > tmp-ver.out 2>&1
+  else		
+    ${CM_SYS_UTIL_VERSION_CMD}  > tmp-ver.out
+  fi
+  echo ${CM_SYS_UTIL_VERSION_CMD}
+  echo $PWD
 fi
 
 test $? -eq 0 || exit $?
