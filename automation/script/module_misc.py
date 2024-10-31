44be9bd3fb9948d8be29908be26eecd8ee24e2b6
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-11-01 00:45:48 +0530

    update the env key to run command

--- automation/script/module_misc.py
+++ automation/script/module_misc.py
@@ -1916,11 +1916,11 @@ def docker(i):
             mounts[index] = new_host_mount+":"+new_container_mount
             if host_env_key:
                 container_env_string += " --env.{}={} ".format(host_env_key, container_env_key)
-                # check if the below lines are needed when inputs are mapped to container paths
         
                 for v in docker_input_mapping:
                     if docker_input_mapping[v] == host_env_key:
                         i[v] = container_env_key 
+                        i_run_cmd[v] = container_env_key
         
         mounts = list(filter(lambda item: item is not None, mounts))
 
