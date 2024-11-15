4be54b38ef341f7a1254bc8f66fe050a4fbae9a4
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-11-15 13:49:13 +0530

    update env

--- script/get-dataset-mixtral/customize.py
+++ script/get-dataset-mixtral/customize.py
@@ -18,4 +18,7 @@ def postprocess(i):
     
     env['CM_DATASET_MIXTRAL_PREPROCESSED_PATH'] = env['CM_DATASET_PREPROCESSED_PATH']
 
+    if env.get('CM_DATASET_MIXTRAL_GENERATE_TEST_DATA', '') == "yes":
+        env['CM_DATASET_MIXTRAL_PREPROCESSED_PATH'] = env['CM_DATASET_MIXTRAL_TEST_DATA_GENERATED_PATH']
+
     return {'return':0}
