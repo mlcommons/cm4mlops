01212f0b75bd28ad80cf5631ac94c029e7681d8a
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-11-25 14:29:52 +0530

    add find_links url

--- script/get-generic-python-lib/customize.py
+++ script/get-generic-python-lib/customize.py
@@ -104,6 +104,12 @@ def preprocess(i):
 
                 extra += ' --extra-index-url ' + extra_index_url
 
+            # check find-links
+            find_links_url = env.get('CM_GENERIC_PYTHON_PIP_EXTRA_FIND_LINKS_URL', '').strip()
+
+            if find_links_url != '':
+                extra += ' -f ' + find_links
+                
             # Check update
             if env.get('CM_GENERIC_PYTHON_PIP_UPDATE', '') in [
                     True, 'true', 'yes', 'on']:
