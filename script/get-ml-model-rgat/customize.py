565ab2bfb5b0eb5cce7dc8e7433c3ada3a48a4c4
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-12-09 21:16:08 +0530

    update env variable

--- script/get-ml-model-rgat/customize.py
+++ script/get-ml-model-rgat/customize.py
@@ -19,12 +19,12 @@ def postprocess(i):
 
     env = i['env']
 
-    if env.get('RGAT_CHECKPOINT_PATH', '') == '':
-        env['RGAT_CHECKPOINT_PATH'] = os.path.join(
+    if env.get('CM_ML_MODEL_RGAT_CHECKPOINT_PATH', '') == '':
+        env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH'] = os.path.join(
             env['CM_ML_MODEL_PATH'], "RGAT.pt")
     elif env.get('CM_ML_MODEL_PATH', '') == '':
-        env['CM_ML_MODEL_PATH'] = env['RGAT_CHECKPOINT_PATH']
+        env['CM_ML_MODEL_PATH'] = env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH']
 
-    env['CM_GET_DEPENDENT_CACHED_PATH'] = env['RGAT_CHECKPOINT_PATH']
+    env['CM_GET_DEPENDENT_CACHED_PATH'] = env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH']
 
     return {'return': 0}
